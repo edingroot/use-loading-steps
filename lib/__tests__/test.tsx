47a0ac5150eb6ed14aee27f53892d2a744c04db8
Chi Chang <edingroot@gmail.com>
2022-05-10 18:57:42 +0800

    Add unit tests

---
+++ lib/__tests__/test.tsx
@@ -0,0 +1,326 @@
+import 'regenerator-runtime/runtime';
+import {isDone, isLoading, isReloading, LState, useLoadingSteps} from "../index";
+import React, {useMemo} from "react";
+import {createTestComponent, noop, wait} from "../test_util";
+
+describe('no delay', () => {
+    it('initial loaded = true', async () =>  {
+        await createTestComponent(() => {
+            const [loadingState] = useLoadingSteps(0, true);
+            expect(loadingState).toEqual(LState.DONE);
+        });
+    });
+
+    it('initial loaded = true, setSetDone', async () =>  {
+        let expectedState: LState = LState.DONE;
+
+        await createTestComponent(() => {
+            const [loadingState, setStepDone] = useLoadingSteps(1, true);
+
+            useMemo(() => expect(loadingState).toEqual(expectedState), [loadingState]);
+            setStepDone('step1');
+        });
+    });
+
+    it('initial loaded = true, redundant setSetDone', async () =>  {
+        let expectedState: LState = LState.DONE;
+
+        await createTestComponent(() => {
+            const [loadingState, setStepDone] = useLoadingSteps(1, true);
+
+            useMemo(() => expect(loadingState).toEqual(expectedState), [loadingState]);
+            setStepDone('step1');
+            setStepDone('step1');
+            setStepDone('step2');
+        });
+    });
+
+    it('single step', async () =>  {
+        let expectedState: LState;
+        let setStepDoneCb: (s: string) => void = noop;
+
+        // Do before creating test component
+        const doSteps = async () => {
+            expectedState = LState.LOADING;
+            await wait(10); // pause shortly for event loop to run component logics
+            expectedState = LState.DONE;
+            setStepDoneCb('step1');
+        }
+        doSteps().then();
+
+        await createTestComponent(() => {
+            const [loadingState, setStepDone] = useLoadingSteps(1);
+            setStepDoneCb = setStepDone;
+            useMemo(() => expect(loadingState).toEqual(expectedState), [loadingState]);
+        });
+    });
+
+    it('multiple steps', async () =>  {
+        const stepCnt = 3;
+        let expectedState: LState;
+        let setStepDoneCb: (s: string) => void = noop;
+
+        // Do before creating test component
+        const doSteps = async () => {
+            expectedState = LState.LOADING;
+
+            for (let i = 0; i < stepCnt; i++) {
+                if (i == stepCnt - 1) {
+                    expectedState = LState.DONE;
+                }
+                setStepDoneCb(`step${i + 1}`);
+                await wait(10); // pause shortly for event loop to run component logics
+            }
+        }
+        doSteps().then();
+
+        await createTestComponent(() => {
+            const [loadingState, setStepDone] = useLoadingSteps(stepCnt, false);
+            setStepDoneCb = setStepDone;
+            useMemo(() => expect(loadingState).toEqual(expectedState), [loadingState]);
+        });
+    });
+});
+
+describe('with render delay', () => {
+    const renderDelay = 150;
+
+    it('initial loaded = true', async () => {
+        await createTestComponent(() => {
+            const [loadingState] = useLoadingSteps(0, true, {
+                renderDelay: renderDelay,
+            });
+            useMemo(() => expect(loadingState).toEqual(LState.DONE), [loadingState]);
+        });
+    });
+
+    it('single step', async () => {
+        let expectedState: LState;
+        let setStepDoneCb: (s: string) => void = noop;
+
+        // Do before creating test component
+        const doSteps = async () => {
+            expectedState = LState.SILENT_LOADING;
+
+            await wait(renderDelay - 10);
+            expectedState = LState.LOADING;
+
+            await wait(100);
+            expectedState = LState.DONE;
+            setStepDoneCb('step1');
+        }
+        doSteps().then();
+
+        await createTestComponent(() => {
+            const [loadingState, setStepDone] = useLoadingSteps(1, false, {
+                renderDelay: renderDelay,
+            });
+            setStepDoneCb = setStepDone;
+            useMemo(() => expect(loadingState).toEqual(loadingState), [loadingState]);
+        }, 300);
+    });
+
+    it('multiple steps', async () => {
+        let expectedState: LState;
+        let setStepDoneCb: (s: string) => void = noop;
+
+        // Do before creating test component
+        const doSteps = async () => {
+            expectedState = LState.SILENT_LOADING;
+
+            await wait(renderDelay - 10);
+            expectedState = LState.LOADING;
+
+            await wait(100);
+            setStepDoneCb('step1');
+
+            await wait(100);
+            expectedState = LState.DONE;
+            setStepDoneCb('step2');
+        }
+        doSteps().then();
+
+        await createTestComponent(() => {
+            const [loadingState, setStepDone] = useLoadingSteps(2, false, {
+                renderDelay: renderDelay,
+            });
+            setStepDoneCb = setStepDone;
+            useMemo(() => expect(loadingState).toEqual(expectedState), [loadingState]);
+        }, 400);
+    });
+
+    it('multiple steps: done before render delay timeout', async () => {
+        let expectedState: LState;
+        let setStepDoneCb: (s: string) => void = noop;
+
+        // Do before creating test component
+        const doSteps = async () => {
+            expectedState = LState.SILENT_LOADING;
+
+            await wait(10);
+            setStepDoneCb('step1');
+
+            await wait(10);
+            expectedState = LState.DONE;
+            setStepDoneCb('step2');
+
+            await wait(renderDelay - 10);
+        }
+        doSteps().then();
+
+        await createTestComponent(() => {
+            const [loadingState, setStepDone] = useLoadingSteps(2, false, {
+                renderDelay: renderDelay,
+            });
+            setStepDoneCb = setStepDone;
+            useMemo(() => expect(loadingState).toEqual(expectedState), [loadingState]);
+        });
+    });
+});
+
+describe('with reset delay', () => {
+    const resetDelay = 150;
+
+    it('multiple steps', async () => {
+        let expectedState: LState;
+        let setStepDoneCb: (s: string) => void = noop;
+        let resetLoadingCb: () => void = noop;
+
+        // Do before creating test component
+        const doSteps = async () => {
+            expectedState = LState.LOADING;
+
+            await wait(100);
+            setStepDoneCb('step1');
+
+            await wait(100);
+            expectedState = LState.DONE;
+            setStepDoneCb('step2');
+
+            await wait(100);
+            expectedState = LState.SILENT_LOADING;
+            resetLoadingCb();
+
+            await wait(resetDelay - 10);
+            expectedState = LState.LOADING;
+
+            await wait(100);
+            expectedState = LState.DONE;
+            setStepDoneCb('step1');
+            setStepDoneCb('step2');
+        }
+        doSteps().then();
+
+        await createTestComponent(() => {
+            const [loadingState, setStepDone, resetLoading] = useLoadingSteps(2, false, {
+                resetDelay: resetDelay,
+            });
+            setStepDoneCb = setStepDone;
+            resetLoadingCb = resetLoading;
+            useMemo(() => expect(loadingState).toEqual(expectedState), [loadingState]);
+        }, 500);
+    });
+});
+
+describe('with render delay + reset delay', () => {
+    const renderDelay = 100;
+    const resetDelay = 150;
+
+    it('multiple steps', async () => {
+        let expectedState: LState;
+        let setStepDoneCb: (s: string) => void = noop;
+        let resetLoadingCb: () => void = noop;
+
+        // Do before creating test component
+        const doSteps = async () => {
+            expectedState = LState.SILENT_LOADING;
+
+            await wait(renderDelay - 10);
+            expectedState = LState.LOADING;
+
+            await wait(50);
+            setStepDoneCb('step1');
+
+            await wait(50);
+            expectedState = LState.DONE;
+            setStepDoneCb('step2');
+
+            await wait(50);
+            expectedState = LState.SILENT_LOADING;
+            resetLoadingCb();
+
+            await wait(resetDelay - 10);
+            expectedState = LState.LOADING;
+
+            await wait(50);
+            expectedState = LState.DONE;
+            setStepDoneCb('step1');
+            setStepDoneCb('step2');
+        }
+        doSteps().then();
+
+        await createTestComponent(() => {
+            const [loadingState, setStepDone, resetLoading] = useLoadingSteps(2, false, {
+                renderDelay: renderDelay,
+                resetDelay: resetDelay,
+            });
+            setStepDoneCb = setStepDone;
+            resetLoadingCb = resetLoading;
+            useMemo(() => expect(loadingState).toEqual(expectedState), [loadingState]);
+        }, 500);
+    });
+});
+
+describe('with done delay', () => {
+    const doneDelay = 150;
+
+    it('multiple steps', async () => {
+        let expectedState: LState;
+        let setStepDoneCb: (s: string) => void = noop;
+
+        // Do before creating test component
+        const doSteps = async () => {
+            expectedState = LState.LOADING;
+
+            await wait(10); // pause shortly for event loop to run component logics
+            expectedState = LState.DELAY_DONE;
+            setStepDoneCb('step1');
+            setStepDoneCb('step2');
+
+            await wait(doneDelay - 10);
+            expectedState = LState.DONE;
+        }
+        doSteps().then();
+
+        await createTestComponent(() => {
+            const [loadingState, setStepDone] = useLoadingSteps(2, false, {
+                doneDelay: doneDelay,
+            });
+            setStepDoneCb = setStepDone;
+            useMemo(() => expect(loadingState).toEqual(expectedState), [loadingState]);
+        }, 300);
+    });
+});
+
+describe('util functions', () => {
+    it('isLoading', async () => {
+        const positiveTargets = [LState.LOADING];
+        const negativeTargets = [LState.SILENT_LOADING, LState.DELAY_DONE, LState.DONE];
+        positiveTargets.forEach(v => expect(isLoading(v)).toEqual(true));
+        negativeTargets.forEach(v => expect(isLoading(v)).toEqual(false));
+    });
+
+    it('isReloading', async () => {
+        const positiveTargets = [LState.SILENT_LOADING, LState.LOADING];
+        const negativeTargets = [LState.DELAY_DONE, LState.DONE];
+        positiveTargets.forEach(v => expect(isReloading(v)).toEqual(true));
+        negativeTargets.forEach(v => expect(isReloading(v)).toEqual(false));
+    });
+
+    it('isDone', async () => {
+        const positiveTargets = [LState.DONE];
+        const negativeTargets = [LState.SILENT_LOADING, LState.LOADING, LState.DELAY_DONE];
+        positiveTargets.forEach(v => expect(isDone(v)).toEqual(true));
+        negativeTargets.forEach(v => expect(isDone(v)).toEqual(false));
+    });
+});
---
+++ lib/test_util.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import renderer, {act} from "react-test-renderer";
+import {LState} from "./index";
+
+export async function wait(period: number): Promise<void> {
+    return period > 0 ? new Promise(resolve => setTimeout(resolve, period)) : Promise.resolve();
+}
+
+export function noop() {}
+
+export async function createTestComponent(logic: () => void, waitToFinish = 100) {
+    const Component = ({}): React.ReactElement | null => {
+        logic();
+        return null;
+    };
+    await act(async () => {
+        renderer.create(<Component/>);
+        await wait(waitToFinish);
+    });
+}
